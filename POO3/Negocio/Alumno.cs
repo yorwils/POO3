4ae3f03ca5cb371ae6e10cb4e7873c840cfaa199
yorwils <yorwi_torres_123@hotmail.com>
2022-03-12 00:25:37 -0500

    octavo trabajo

---
+++ POO3/Negocio/Alumno.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO3.Negocio
+{
+    class Alumno
+    {
+        public String DNI { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string sexo { get; set; }
+       
+
+        public int promediar(int n1, int n2, int n3)
+        {
+            int promedio = (n1 + n2 + n3) / 3;
+            return promedio;
+        }
+
+
+        public void limpiar()
+        {
+            Console.Clear();
+        }
+        public void Mostrardatos()
+        {
+            Console.WriteLine("-----------datos alumno------");
+            Console.WriteLine("DNI :" + this.DNI);
+            Console.WriteLine("Nombres :" + this.Nombres);
+            Console.WriteLine("APELLIDOS :" + this.Apellidos);
+            Console.WriteLine("SEXO : " + this.sexo);
+           
+        }
+
+        public string GenerarCodigo(int n1, int n2, int n3)
+        {
+            String codigo = this.DNI.Substring(0, 5) + this.Apellidos.Substring(0, 3);
+            return codigo;
+        }
+
+      
+        public string leerNota(int nota)
+        {
+            String notaTexto = null;
+            if (nota == 0)
+            {
+                notaTexto = "cero";
+
+            }
+            else if (nota == 1)
+            {
+                notaTexto = "uno ";
+
+            }
+            else if (nota == 2)
+            {
+                notaTexto = "dos";
+
+            }
+            else if (nota == 3)
+            {
+                notaTexto = "tres ";
+
+            }
+            else if (nota == 4)
+            {
+                notaTexto = "Cuatro";
+
+            }
+            else if (nota == 5)
+            {
+                notaTexto = "Cinco";
+            }
+            else if (nota == 6)
+            {
+                notaTexto = "seis";
+
+            }
+            else if (nota == 7)
+            {
+                notaTexto = "siete";
+            }
+            else if (nota == 8)
+            {
+                notaTexto = " ocho";
+            }
+            else if (nota == 9)
+            {
+                notaTexto = " nieve";
+            }
+            else if (nota == 10)
+            {
+                notaTexto = "Dies";
+            }
+            else if (nota == 11)
+            {
+             notaTexto = "once";
+            }  
+
+             else if (nota == 12)
+            {
+            notaTexto = "Doce";
+
+            }
+            else if (nota == 13)
+            {
+                notaTexto = "Trece";
+
+            }
+            else if (nota == 14)
+            {
+                notaTexto = "Catorce";
+
+            }
+            else if (nota == 15)
+            {
+                notaTexto = "Quince";
+
+            }
+            else if (nota == 16)
+            {
+                notaTexto = "Diesisesis";
+
+            }
+            else if (nota == 17)
+            {
+                notaTexto = "diecicitete";
+
+            }
+            else if (nota == 18)
+            {
+                notaTexto = "Dociciocho";
+
+            }
+            else if (nota == 19)
+            {
+                notaTexto = " diecinueve";
+            }
+            else if (nota == 20)
+            {
+                notaTexto = "venite";
+
+            }
+
+            return notaTexto;
+        
+        }
+    }
+}
---
+++ POO3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POO3.Negocio;
+
+namespace POO3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno();
+
+            Console.WriteLine("ingese el dni");
+            alumno.DNI = Console.ReadLine();
+            Console.WriteLine("ingrese el Nombres: ");
+            alumno.Nombres = Console.ReadLine();
+            Console.WriteLine("ingrese apellidos: ");
+            alumno.Apellidos = Console.ReadLine();
+            Console.WriteLine("ingrese sexo");
+            alumno.sexo = Console.ReadLine();
+
+
+            Console.WriteLine("ingres nota 1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese nota 2");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese nota 3");
+            int n3 = int.Parse(Console.ReadLine());
+            alumno.limpiar();
+            alumno.Mostrardatos();
+            alumno.leerNota(0);
+            Console.WriteLine(" promedio: " + alumno.promediar(n1, n2, n3));
+         int nota = 0;  
+            Console.WriteLine("leer nota :" + alumno.leerNota(nota));
+            
+        }
+
+    }
+}
